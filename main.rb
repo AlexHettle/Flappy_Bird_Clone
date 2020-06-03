4487f424602edb77d0ba7cef166c2e950442f38a
Alex Hettle <51217513+AlexHettle@users.noreply.github.com>
2020-06-03 14:53:24 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,62 +1,66 @@
 require 'ruby2d'
-set title: "Flappy Bird",background: '#29f8ff', width: 500, height: 500
-Groundpipe=Rectangle.new(y:350, x:600,width:100,height:rand(200..400),color: 'green')
-Ground=Rectangle.new(y: 480,width: 500, height: 20,color:'brown')
-Ceilingpipe=Rectangle.new(y:0,x:900,width:100,height:rand(50..400),color:'green')
-Ceiling=Rectangle.new(y: 20,width: 500, height: 20,color:'brown')
-Bird=Circle.new(x:200,y:250,radius:25,color: 'yellow')
-def play_game()
-  canjump=1
-  score=0
-  def lose_game(score)
+set title: "Flappy Ball",background: '#29f8ff', width: 500, height: 500
+def start_screen()
+  start_text=Text.new("FLAPPY BALL",x:100,y:250,color:"black",size:50)
+  button=Rectangle.new(x:130,y:320,width:250,height:70,color:'green')
+  start=Text.new("START",x:170,y:330,color:"black",size:50)
+  on :mouse_down do |event|
+    if button.contains? event.x,event.y
     clear
-    Ground.add
-    Bird.color='red'
-    Bird.add
-    score_text="SCORE: "+score.to_s
-    t1=Text.new("GAME OVER",x:100,y:250,color:"black",size:50)
-    t2=Text.new(score_text,x:100,y:290,color:"black",size:50)
-    return 0
-  end
-  on :key_down do |event|
-    if canjump==1
-      Bird.y-=60
-    end
-  end
-  score_num="SCORE: "+score.to_s
-  score_text=Text.new(score_num,x:50,y:50,color:'black',size:20)
-  update do
-    score_text.remove
-    if canjump==1
-      score_num="SCORE: "+score.to_s
-      score_text=Text.new(score_num,x:50,y:50,color:'black',size:20)
-    end
-    if canjump==1
-      score+=1
-    end
-    if Bird.y<455
-      Bird.y+=4
-    end
-    if Bird.y<65
-      Bird.y=65
-    end
-    Groundpipe.x-=4
-    Ceilingpipe.x-=4
-    if Groundpipe.x<=-100
-      Groundpipe.x=500
-      Groundpipe.y=rand(300..400)
-    end
-    if Ceilingpipe.x<=-100
-      Ceilingpipe.x=500
-      Ceilingpipe.height=rand(200..400)
-    end
-    if (Groundpipe.contains? Bird.x+20,Bird.y+20) || (Groundpipe.contains? Bird.x,Bird.y+20) || (Groundpipe.contains? Bird.x-20,Bird.y+20)
-      canjump=lose_game(score)
-    end
-    if (Ceilingpipe.contains? Bird.x+20,Bird.y-20) || (Ceilingpipe.contains? Bird.x,Bird.y-20) || (Ceilingpipe.contains? Bird.x-20,Bird.y-20)
-      canjump=lose_game(score)
+    play_game()
     end
   end
 end
-play_game()
+def play_game()
+  groundpipe=Rectangle.new(y:350, x:600,width:100,height:rand(300..400),color: 'green')
+  ground=Rectangle.new(y: 480,width: 500, height: 20,color:'brown')
+  ceilingpipe=Rectangle.new(y:0,x:900,width:100,height:rand(50..400),color:'green')
+  ceiling=Rectangle.new(y: 20,width: 500, height: 20,color:'brown')
+  bird=Circle.new(x:200,y:250,radius:25,color: 'yellow')
+  canjump=1
+  score=0
+  score_text=Text.new("SCORE: "+score.to_s,x:50,y:50,color:'black',size:20)
+  on :key_down do
+    if canjump==1
+      bird.y-=60
+    end
+  end
+  update do
+    if canjump==1
+      score_text.remove
+      score_text=Text.new("SCORE: "+score.to_s,x:50,y:50,color:'black',size:20)
+      score+=1
+    end
+    if bird.y<455
+      bird.y+=4
+    end
+    if bird.y<65
+      bird.y=65
+    end
+    groundpipe.x-=4
+    ceilingpipe.x-=4
+    if groundpipe.x<=-100
+      groundpipe.x=500
+      groundpipe.y=rand(300..400)
+    end
+    if ceilingpipe.x<=-100
+      ceilingpipe.x=500
+      ceilingpipe.height=rand(200..400)
+    end
+    if (ceilingpipe.contains? bird.x+20,bird.y-20) || (ceilingpipe.contains? bird.x,bird.y-20) ||
+       (ceilingpipe.contains? bird.x-20,bird.y-20) || (groundpipe.contains? bird.x+20,bird.y+20) ||
+       (groundpipe.contains? bird.x,bird.y+20) || (groundpipe.contains? bird.x-20,bird.y+20)
+      clear
+      ground.add
+      bird.color='red'
+      ceiling.add
+      bird.add
+      score_text="SCORE: "+score.to_s
+      t1=Text.new("GAME OVER",x:100,y:250,color:"black",size:50)
+      t2=Text.new(score_text,x:100,y:290,color:"black",size:50)
+      canjump=0
+    end
+  end
+end
+start_screen()
 show
